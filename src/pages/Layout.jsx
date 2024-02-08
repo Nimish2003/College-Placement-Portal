65c7b0a421e8689c2127dd4ab8d65365aae03be7
NimishP08 <113787407+NimishP08@users.noreply.github.com>
2024-02-08 12:20:50 +0530

    Initial commit

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ResponsiveAppBar from '../components/ResponsiveAppBar'
+import Footer  from '../components/Footer'
+
+const Layout = () => {
+  return (
+    <div>
+      <ResponsiveAppBar />
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
