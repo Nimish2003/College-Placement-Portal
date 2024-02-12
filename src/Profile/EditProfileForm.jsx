e9e31576323fbe0fd96de4de3f6d011ad8a7b1d9
NimishP08 <113787407+NimishP08@users.noreply.github.com>
2024-02-12 12:50:57 +0530

    Home Page

--- src/Profile/EditProfileForm.jsx
+++ src/Profile/EditProfileForm.jsx
@@ -109,7 +109,7 @@ export function EditProfileForm() {
               type="button"
               className="w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
             >
-              Save
+              Edit
             </button>
           </div>
         </div>
