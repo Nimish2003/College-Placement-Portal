6653cdae4f9194774eab6d4313b901f6b9774e25
Mithali Nayak <101163706+mithalinayak@users.noreply.github.com>
2024-03-05 19:06:48 +0530

    academic done

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Sidebar from "./SideBar";
 import { EditProfileForm } from "./EditProfileForm";
-import { AcademicDetails } from "./AcademicDetails";
 
 const Profile = () => {
   return (
     <div className="flex">
       <Sidebar />
       <EditProfileForm />
-      <AcademicDetails />
     </div>
   );
 };
