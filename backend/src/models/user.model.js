3568c90ab3c241736fc0f7093d00cb087e4247d3
Prathameshgp48 <120002929+Prathameshgp48@users.noreply.github.com>
2024-03-10 18:22:02 +0530

    data modelling

--- backend/src/models/user.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const UserSchema = new Schema({
-    userType: {
-        
-    }
-}, { timestamps: true });
-
-export const User = mongoose.model("User", UserSchema);
---
+++ backend/src/models/user/user.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      index: true, //to make field searchable in optimized manner
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+        if (!/\S+@\S+\.\S+/.test(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    phone: {
+      countryCode: Number,
+      number: String,
+    },
+    address: {
+      streetAddress: String,
+      apartment: String,
+      city: String,
+      state: String,
+      zip: String,
+    },
+    dateOfBirth: Date,
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+    },
+    profile_pic: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    academics: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Academics",
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", UserSchema);
