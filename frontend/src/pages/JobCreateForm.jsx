397755ce8bb711faf45020fdb1a34c8bf7424431
Prathameshgp48 <120002929+Prathameshgp48@users.noreply.github.com>
2024-03-11 20:03:05 +0530

    job posting route

---
+++ frontend/src/pages/JobCreateForm.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+
+export default function JobPostingForm() {
+  return (
+    <div className="mx-auto w-full max-w-7xl bg-slate-100 py-2">
+      <div className="mx-auto my-4 max-w-2xl md:my-6">
+        {/* Form */}
+        <div className="overflow-hidden rounded-xl bg-white p-4 shadow">
+          <p className="text-sm font-bold text-gray-900">Job Posting Details</p>
+          <div className="mt-6 gap-6 space-y-4 md:grid md:grid-cols-2 md:space-y-0">
+            <div className="w-full">
+              <label
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                htmlFor="jobTitle"
+              >
+                Job Title
+              </label>
+              <input
+                className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                type="text"
+                placeholder="Enter job title"
+                id="jobTitle"
+              ></input>
+            </div>
+
+            <div className="w-full">
+              <label
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                htmlFor="companyName"
+              >
+                Company Name
+              </label>
+              <input
+                className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                type="text"
+                placeholder="Enter company name"
+                id="companyName"
+              ></input>
+            </div>
+            
+            <div className="w-full">
+              <label
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                htmlFor="applicationDeadline"
+              >
+                Application Deadline
+              </label>
+              <input
+                className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                type="date"
+                id="applicationDeadline"
+              ></input>
+            </div>
+
+            <div className="col-span-2 grid">
+              <div className="w-full">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="jobResponsibilities"
+                >
+                  Job Responsibilities
+                </label>
+                <textarea
+                  className="flex h-40 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                  placeholder="Enter job responsibilities"
+                  id="jobResponsibilities"
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="col-span-2 grid">
+              <div className="w-full">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="qualificationsRequired"
+                >
+                  Qualifications Required
+                </label>
+                <textarea
+                  className="flex h-40 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                  placeholder="Enter qualifications required"
+                  id="qualificationsRequired"
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="col-span-2 grid">
+              <div className="w-full">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="preferredSkills"
+                >
+                  Preferred Skills
+                </label>
+                <textarea
+                  className="flex h-40 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                  placeholder="Enter preferred skills"
+                  id="preferredSkills"
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="col-span-2 grid">
+              <button
+                type="button"
+                className="w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+              >
+                Post Job
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
