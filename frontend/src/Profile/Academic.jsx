d09d7a28827e868d19cd82c67c013367da9c77b1
Mithali Nayak <101163706+mithalinayak@users.noreply.github.com>
2024-03-21 18:55:58 +0530

    Academic form changes

--- frontend/src/Profile/Academic.jsx
+++ frontend/src/Profile/Academic.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import MyComponent from "./MyCompenent";
+import MyComponent1 from "./MyComponent1";
 
 export default function Academic() {
   const [educationType, setEducationType] = useState("");
@@ -14,6 +16,24 @@ export default function Academic() {
         <div className="overflow-hidden rounded-xl bg-white p-4 shadow">
           <p className="text-sm font-bold text-gray-900">Academic Details</p>
           <div className="mt-6 gap-6 space-y-4 md:grid md:grid-cols-2 md:space-y-0">
+            
+          <div className="col-span-2 grid">
+              <div className="w-full">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="board10thMarks"
+                >
+                    10th Standard Marks
+                </label>
+                <input
+                  className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                  type="text"
+                  placeholder="Enter 10th Standard Marks"
+                  id="board10thMarks"
+                ></input>
+              </div>
+            </div>
+            
             <div className="col-span-2 grid">
               {/* Dropdown to select education type */}
               <div className="w-full mb-4">
@@ -21,11 +41,11 @@ export default function Academic() {
                   htmlFor="educationType"
                   className="block text-sm font-medium leading-none"
                 >
-                  Select Previous Education Type
+                  12th/Diploma
                 </label>
                 <select
                   id="educationType"
-                  className="mt-1 block w-full pl-3 pr-10 py-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-black/30 focus:border-black/30"
+                  className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                   value={educationType}
                   onChange={handleEducationTypeChange}
                 >
@@ -58,21 +78,6 @@ export default function Academic() {
 
               {educationType === "board" && (
                 <div className="w-full mb-4">
-                  {/* Input fields for board */}
-                  {/* Add your input fields for board details here */}
-                  <label
-                    htmlFor="board10thMarks"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    10th Standard Marks
-                  </label>
-                  <input
-                    type="text"
-                    id="board10thMarks"
-                    className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                    placeholder="Enter 10th Standard Marks"
-                  />
-                  {/* Add more input fields for board details here */}
                   <label
                     htmlFor="board12thMarks"
                     className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -88,36 +93,6 @@ export default function Academic() {
                   {/* Add more input fields for board details here */}
                 </div>
               )}
-            </div>
-
-            <div className="w-full">
-              <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                htmlFor="year"
-              >
-                Year of Study
-              </label>
-              <input
-                className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                type="text"
-                placeholder="Enter year of study"
-                id="year"
-              ></input>
-            </div>
-
-            <div className="w-full">
-              <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                htmlFor="Semester"
-              >
-                Semester
-              </label>
-              <input
-                className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                type="text"
-                placeholder="Enter your semester"
-                id="semester"
-              ></input>
             </div>
 
             <div className="col-span-2 grid">
@@ -137,36 +112,14 @@ export default function Academic() {
               </div>
             </div>
 
-            <div className="w-full">
-              <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                htmlFor="firstse"
-              >
-                Year of Study
-              </label>
-              <input
-                className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                type="text"
-                placeholder="Enter "
-                id="firstYearPercentage1"
-              ></input>
+            <div className="col-span-2 grid">
+              <MyComponent1 />
             </div>
 
-            <div className="w-full">
-              <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                htmlFor="Semester"
-              >
-                Semester
-              </label>
-              <input
-                className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                type="text"
-                placeholder="Enter your semester"
-                id="semester"
-              ></input>
+            <div className="col-span-2 grid">
+              <p className="text-sm font-bold text-gray-900 mb-4">Backlogs</p>
+              <MyComponent />
             </div>
-
 
             <div className="col-span-2 grid">
               <button
---
+++ frontend/src/Profile/MyComponent1.jsx
@@ -0,0 +1,317 @@
+import React, { useState } from "react";
+
+function MyComponent1() {
+  const [yearOfStudy, setYearOfStudy] = useState("");
+
+  const handleYearOfStudyChange = (e) => {
+    setYearOfStudy(e.target.value);
+  };
+
+  const renderInputFields = () => {
+    switch (yearOfStudy) {
+      case "1st year":
+        return (
+          <>
+          </>
+        );
+      case "2nd year":
+        return (
+          <> 
+          <div className="w-full mb-4">
+          <label htmlFor="1stSemGPA" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            1st Semester GPA
+          </label>
+          <input
+            type="text"
+            id="1stSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 1st Semester GPA"
+          />
+          <label htmlFor="1stSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="1stSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+        <div className="w-full mb-4">
+          <label htmlFor="2ndSemGPA" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            2nd Semester GPA
+          </label>
+          <input
+            type="text"
+            id="2ndSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 2nd Semester GPA"
+          />
+          <label htmlFor="2ndSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="2ndSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+            {/* Input fields for 2nd year */}
+            {/* Similar to above, add input fields for 3rd and 4th semesters */}
+          </>
+        );
+      case "3rd year":
+        return (
+          <>
+           <div className="w-full mb-4">
+          <label htmlFor="1stSemGPA" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            1st Semester GPA
+          </label>
+          <input
+            type="text"
+            id="1stSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 1st Semester GPA"
+          />
+          <label htmlFor="1stSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="1stSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+        <div className="w-full mb-4">
+          <label htmlFor="2ndSemGPA" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            2nd Semester GPA
+          </label>
+          <input
+            type="text"
+            id="2ndSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 2nd Semester GPA"
+          />
+          <label htmlFor="2ndSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="2ndSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+
+        <div className="w-full mb-4">
+          <label htmlFor="3rdSemGPA" 
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            3rd Semester GPA
+          </label>
+          <input
+            type="text"
+            id="3rdSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 3rd Semester GPA"
+          />
+          <label htmlFor="3rdSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="3rdSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+        <div className="w-full mb-4">
+          <label htmlFor="4thSemGPA" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            4th Semester GPA
+          </label>
+          <input
+            type="text"
+            id="4thSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 4th Semester GPA"
+          />
+          <label htmlFor="4thSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="4thSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+            {/* Input fields for 3rd year */}
+            {/* Similar to above, add input fields for 5th and 6th semesters */}
+          </>
+        );
+      case "4th year":
+        return (
+          <>
+          <div className="w-full mb-4">
+          <label htmlFor="1stSemGPA" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            1st Semester GPA
+          </label>
+          <input
+            type="text"
+            id="1stSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 1st Semester GPA"
+          />
+          <label htmlFor="1stSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="1stSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+        <div className="w-full mb-4">
+          <label htmlFor="2ndSemGPA" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            2nd Semester GPA
+          </label>
+          <input
+            type="text"
+            id="2ndSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 2nd Semester GPA"
+          />
+          <label htmlFor="2ndSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="2ndSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+
+        <div className="w-full mb-4">
+          <label htmlFor="3rdSemGPA" 
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            3rd Semester GPA
+          </label>
+          <input
+            type="text"
+            id="3rdSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 3rd Semester GPA"
+          />
+          <label htmlFor="3rdSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="3rdSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+        <div className="w-full mb-4">
+          <label htmlFor="4thSemGPA" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            4th Semester GPA
+          </label>
+          <input
+            type="text"
+            id="4thSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 4th Semester GPA"
+          />
+          <label htmlFor="4thSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="4thSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+
+        <div className="w-full mb-4">
+          <label htmlFor="5thSemGPA" 
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            5th Semester GPA
+          </label>
+          <input
+            type="text"
+            id="5thSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 5th Semester GPA"
+          />
+          <label htmlFor="5thSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="5thPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+        <div className="w-full mb-4">
+          <label htmlFor="6thSemGPA" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            6th Semester GPA
+          </label>
+          <input
+            type="text"
+            id="6thSemGPA"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter 6th Semester GPA"
+          />
+          <label htmlFor="6thSemPercentage" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Percentage
+          </label>
+          <input
+            type="text"
+            id="6thSemPercentage"
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="Enter Percentage"
+          />
+        </div>
+            {/* Input fields for 4th year */}
+            {/* Similar to above, add input fields for 7th and 8th semesters */}
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="col-span-2 grid">
+      <div className="w-full mb-4">
+        <label htmlFor="year" className="block text-sm font-medium leading-none">
+          Year of Study
+        </label>
+        <select
+          id="year"
+          className="mt-1 block w-full pl-3 pr-10 py-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-black/30 focus:border-black/30"
+          value={yearOfStudy}
+          onChange={handleYearOfStudyChange}
+        >
+          <option value="">Select Year of Study</option>
+          <option value="1st year">1st year</option>
+          <option value="2nd year">2nd year</option>
+          <option value="3rd year">3rd year</option>
+          <option value="4th year">4th year</option>
+        </select>
+      </div>
+      <div> 
+        <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 mb-4"> Enter your semester GPA</p>
+      </div>
+      
+      {renderInputFields()}
+    </div>
+  );
+}
+
+export default MyComponent1;
