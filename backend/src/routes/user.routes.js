c8d6f54aa23fdcd980591c07c37a7bf62016f06a
Prathameshgp48 <120002929+Prathameshgp48@users.noreply.github.com>
2024-03-23 19:31:47 +0530

    The new dependency, multer, is used for handling file uploads in Express.js applications. It was added to enable file upload functionality in the backend application.

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import {
+  download,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "profile_pic",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+//experimental route
+router.route("/download").get(download);
+
+export default router;
