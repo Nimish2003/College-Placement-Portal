612394c257fc4aba8d0ca8413d6ec1595f5db1bc
Mithali Nayak <101163706+mithalinayak@users.noreply.github.com>
2024-04-21 21:07:05 +0530

    Admin dashboard

--- frontend/src/Admin/Admin.jsx
+++ frontend/src/Admin/Admin.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import Navbar from './Navbar';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Company from "./Company";
 
 const Admin = () => {
   return (
-    <div>
-      < Navbar/>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+      <div className="flex">
+        <Sidebar />
+        <Switch>
+          <Route path="/company-details">
+            <Company />
+          </Route>
+          {/* Define other routes here */}
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default Admin;
